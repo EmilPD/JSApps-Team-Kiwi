e99835ddc5b958e19b4cf22d084633f532b56050
Emil Dimitrov <emil_132@abv.bg>
2017-05-01 09:32:20 +0300

    modified app.js

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-var express = require('express');
-var app = express();
+let express = require('express'),
+  bodyParser = require('body-parser'),
+  lowdb = require('lowdb');
+
+let db = lowdb('./data/data.json');
+db._.mixin(require('underscore-db'));
+
+let app = express();
+app.use(bodyParser.json());
 
 app.use(express.static('app'));
 app.use('/libs', express.static('node_modules'));
+
+// Categories
+let categoriesController = require('./controllers/categories-controller.js')(db);
+app.get('/api/categories/:id', categoriesController.get);
 
 app.listen(process.env.PORT || 80, function () {
     console.log("App is running on port: 80");
