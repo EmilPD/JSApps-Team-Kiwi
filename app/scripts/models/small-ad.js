8611425db4f70bdf9b86a8191f6ff1f9cfc2ef81
denislav48 <denislavstoyanov48@gmail.com>
2017-05-06 22:44:34 +0300

    added geters and setters plus validations

--- app/scripts/models/small-ad.js
+++ app/scripts/models/small-ad.js
@@ -1,10 +1,33 @@
+import { validator } from '../utils/validator.js';
+
 let SmallAd = (function() {
     class SmallAd {
         constructor(title, description) {
             this.title = title;
             this.description = description;
         }
-		
+
+        get title() {
+            return this._title;
+        }
+
+        set title(value) {
+            validator.validateIfUndefinedOrNull(value, 'Title');
+            validator.validateTypeOf(value, 'Title', 'string');
+            validator.validateIfEmptyString(value, 'Title');
+            this._title = value;
+        }
+
+        get description() {
+			return this._description;
+		}
+        
+		set description(value) {
+            validator.validateIfUndefinedOrNull(value, 'Description');
+			validator.validateTypeOf(value, 'Description', 'string');
+            validator.validateIfEmptyString(value, 'Description');
+			this._description = value;
+		}
     }
 
     return SmallAd;
--- app/scripts/models/user.js
+++ app/scripts/models/user.js
@@ -1,3 +1,6 @@
+import { validator } from '../utils/validator.js';
+//const validator = require('../utils/validator.js');
+
 let User = (function() {
     class User {
         constructor(name, username, password, phone, email) {
@@ -7,6 +10,62 @@ let User = (function() {
             this.phone = phone;
             this.email = email;
         }
+
+        get name() {
+            return this._name;
+        }
+
+        set name(value) {
+            validator.validateIfUndefinedOrNull(value, 'Name');
+            validator.validateTypeOf(value, 'Name', 'string');
+            validator.validateIfEmptyString(value, 'Name');
+            this._name = value;
+        }
+
+        get username() {
+            return this._username;
+        }
+
+        set username(value) {
+            validator.validateIfUndefinedOrNull(value, 'Username');
+            validator.validateUsername(value);
+            validator.validateIfEmptyString(value, 'Username');
+            this._username = value;
+        }
+
+        get password() {
+            return this._password;
+        }
+
+        set password(value) {
+            validator.validateIfUndefinedOrNull(value, 'Password');
+            validator.validatePassword(value);
+            validator.validateIfEmptyString(value, 'Password');
+            this._password = value;
+        }
+
+        get phone() {
+            return this._phone;
+        }
+
+        set phone(value) {
+            validator.validateIfUndefinedOrNull(value, 'Phone');
+            validator.validatePhone(value);
+            validator.validateIfEmptyString(value, 'Phone');
+            this._phone = value;
+        }
+
+        get email() {
+            return this._email;
+        }
+
+        set email(value) {
+            validator.validateIfUndefinedOrNull(value, 'Email');
+            validator.validateEmail(value);
+            validator.validateIfEmptyString(value, 'Email');
+            this._email = value;
+        }
+    }
 
     return User;
 })();
