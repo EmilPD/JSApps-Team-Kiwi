ed3c0841f3774906b15a7912a2445f288771ef94
Emil Dimitrov <emil_132@abv.bg>
2017-05-06 22:55:30 +0300

    fixed login and register and added logout and classifieds

--- app/scripts/data/data.js
+++ app/scripts/data/data.js
@@ -1,33 +1,96 @@
 import { requester } from '../requester.js';
 
 let data = (function() {
+    const LOCALSTORAGE_AUTH_KEY_NAME = 'kiwi-auth-key';
 
-    function getCategory(category) {
-        return requester.get(`/api/categories/${category}`);
+    // Users
+
+    function login(username, password) {
+        const body = {
+            username,
+            password
+        };
+
+        return requester.put('/api/auth/login', body)
+            .then(function(resp) {
+                var user = resp.result;
+                localStorage.setItem(LOCALSTORAGE_AUTH_KEY_NAME, user.authKey);
+                return user;
+            });
+    }
+
+    function register(user) {
+        const body = {
+            name: user.name,
+            username: user.username,
+            password: user.password,
+            phone: user.phone,
+            email: user.email
+        };
+
+        return requester.post('/api/auth/register', body);
+    }
+
+    function logout() {
+        let promise = new Promise(function(resolve, reject) {
+            localStorage.removeItem(LOCALSTORAGE_AUTH_KEY_NAME);
+            resolve();
+        });
+
+        return promise;
+    }
+
+    function hasUser() {
+        return !!localStorage.getItem(LOCALSTORAGE_AUTH_KEY_NAME);
+    }
+
+    // Categories
+
+    function getCategory(categoryName) {
+        return requester.get(`/api/categories/${categoryName}`);
     }
     
-    function login(username, passHash) {
-        const body = {
-            username,
-            passHash
-        };
+    // Classifieds
 
-        return requester.put('/api/auth', body);
+    function getCount() {
+        return requester.get(`/api/classifieds/count`);
     }
 
-    function register(username, passHash) {
+    function getAd(id) {
+        return requester.get(`/api/classifieds/${id}`);
+    }
+
+    function postAd(ad) {
         const body = {
-            username,
-            passHash
+            category: ad.category,
+            title: ad.title,
+            description: ad.description
         };
 
-        return requester.post('/api/users', body);
+        const category = ad.category;
+
+        const headers = {
+            'kiwi-auth-key': localStorage.getItem(LOCALSTORAGE_AUTH_KEY_NAME)
+        };
+
+        return requester.post(`/api/classifieds/${category}`, body, headers);
     }
 
     return {
-        getCategory,
-        login,
-        register
+        users: {
+            login,
+            register,
+            logout,
+            hasUser
+        },
+        categories: {
+            get: getCategory
+        },
+        classifieds: {
+            getCount: getCount,
+            get: getAd,
+            post: postAd
+        }
     };
 
 })();
