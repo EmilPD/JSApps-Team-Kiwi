1ed86d76a3a2e9281f757613288fe9b7f35ef55a
Emil Dimitrov <emil_132@abv.bg>
2017-05-07 23:44:50 +0300

    deleted unneeded module

--- app/scripts/helpers/category-select.js
+++
@@ -1,9 +0,0 @@
-let categorySelect = (function() {
-    $('#categories').change(function () {
-        let category = $(this).val();
-        let url = '/categories/' + category;
-        window.location.hash = url;
-    });
-}());
-
-export { categorySelect };
