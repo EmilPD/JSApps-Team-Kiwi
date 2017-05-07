02f16277fa03cf13bab7ab0ea1eda2a8f1f9a3e0
Emil Dimitrov <emil_132@abv.bg>
2017-05-07 23:48:06 +0300

    a lot of functionality added

--- app/scripts/controllers/template-controller.js
+++ app/scripts/controllers/template-controller.js
@@ -3,7 +3,12 @@ import { requester } from '../requester.js';
 import { data } from '../data/data.js';
 import { handlebarsSubstr } from '../helpers/handlebars-substr.js';
 import { User } from '../models/user.js';
+import { VIPUser } from '../models/vip-user';
 import { SmallAd } from '../models/small-ad.js';
+import { Offer } from '../models/offer.js';
+import { Comment } from '../models/comment.js';
+
+let commentData = data;
 
 let TemplateController = (function() {
     class TemplateController {
@@ -14,41 +19,22 @@ let TemplateController = (function() {
             }
         }
 
-        loadHome() {
-            data.classifieds.getCount()
-            .then((data) => {
-                Promise.all([data, tl.loadTemplate('top')])
-                    .then(([data, template]) => $('#top-container').html(template(data)))
-                    .then(() => {
-                        $('#categories').change(function () {
-                            let category = $(this).val();
-                            let url = '/categories/' + category;
-                            window.location.hash = url;
-                        });
-                    });
-            });
+        loadTopContainer() {
+            Promise.all([data.classifieds.getCount(), tl.loadTemplate('classifiedscount')])
+                .then(([data, template]) => $('#classifieds-count').html(template(data)))
+                .catch(console.log);
+        }
 
-            data.categories.get('all')
-            .then((data) => {
-                Promise.all([data, tl.loadTemplate('home')])
-                    .then(([data, template]) =>  $('#main').html(template(data)))
-                    .catch(console.log);
-            })
+        loadHome() {
+            this.loadTopContainer();
+            Promise.all([data.categories.get('all'), tl.loadTemplate('home')])
+                .then(([data, template]) =>  $('#main').html(template(data)))
+                .catch(console.log);
         }
 
         loadLogin() {
-            data.classifieds.getCount()
-            .then((data) => {
-                Promise.all([data, tl.loadTemplate('top')])
-                    .then(([data, template]) => $('#top-container').html(template(data)))
-                    .then(() => {
-                        $('#categories').change(function () {
-                            let category = $(this).val();
-                            let url = '/categories/' + category;
-                            window.location.hash = url;
-                        });
-                    });
-            });
+            let self = this;
+            this.loadTopContainer();
 
             Promise.all([tl.loadTemplate('login')])
                 .then((template) => $('#main').html(template))
@@ -62,8 +48,7 @@ let TemplateController = (function() {
                         data.users.login(username, password)
                         .then(
                             result => {
-                                $('#login-register-links').addClass('hidden');
-                                $('#logout-link').removeClass('hidden');
+                                self.hideLoginRegister();
                                 toastr.success(`Hi, ${username}`);
                                 location.href = '#/postfreead';
                             },
@@ -75,33 +60,46 @@ let TemplateController = (function() {
         }
 
         loadRegister() {
-            data.classifieds.getCount()
-            .then((data) => {
-                Promise.all([data, tl.loadTemplate('top')])
-                    .then(([data, template]) => $('#top-container').html(template(data)))
-                    .then(() => {
-                        $('#categories').change(function () {
-                            let category = $(this).val();
-                            let url = '/categories/' + category;
-                            window.location.hash = url;
-                        });
-                    });
-            });
+            this.loadTopContainer();
 
             Promise.all([tl.loadTemplate('register')])
                 .then((template) => $('#main').html(template))
                 .then(() => {
+                        let userType = $('#user-type').val;
+                        if (userType === 'normal') {
+                            $('#vip-user-info').addClass('hidden');
+                        }
+                        else if (userType === 'vip') {
+                            $('#vip-user-info').removeClass('hidden');
+                        }
+                        $('#user-type').change(function () {
+                            userType = $(this).val();
+                            if (userType === 'normal') {
+                                $('#vip-user-info').addClass('hidden');
+                            }
+                            else if (userType === 'vip') {
+                                $('#vip-user-info').removeClass('hidden');
+                            }
+                        });
+
                     $('#register-form').on('submit', function(e) {
                         e.preventDefault();
 
+                        userType = $('#user-type').val();
                         const name = $('#input-name').val();
                         const username = $('#input-username').val();
                         const password = $('#input-password').val();
                         const phone = $('#input-phone').val();
                         const email = $('#input-email').val();
 
-                        let newUser = new User(name, username, password, phone, email);
-
+                        let newUser;
+                        if (userType == 'normal') {
+                            newUser = new User(name, username, password);
+                        }
+                        else if (userType == 'vip') {
+                            newUser = new VIPUser(name, username, password, phone, email);
+                        }
+                        
                         data.users.register(newUser)
                         .then(
                             result => {
@@ -138,43 +136,63 @@ let TemplateController = (function() {
                 return;
             }
             else {
-                $('#login-register-links').addClass('hidden');
-                $('#logout-link').removeClass('hidden');
+                this.hideLoginRegister();
             }
 
-            data.classifieds.getCount()
-            .then((data) => {
-                Promise.all([data, tl.loadTemplate('top')])
-                    .then(([data, template]) => $('#top-container').html(template(data)))
-                    .then(() => {
-                        $('#categories').change(function () {
-                            let category = $(this).val();
-                            let url = '/categories/' + category;
-                            window.location.hash = url;
-                        });
-                    });
-            });
+            this.loadTopContainer();
 
             Promise.all([tl.loadTemplate('postfreead')])
                 .then((template) => $('#main').html(template))
                 .then(() => {
+                    let adType = $('#select-ad-type').val;
+                        if (adType === 'smallad') {
+                            $('#offer-info').addClass('hidden');
+                        }
+                        else if (adType === 'offer') {
+                            $('#offer-info').removeClass('hidden');
+                        }
+                        $('#select-ad-type').change(function () {
+                            adType = $(this).val();
+                            if (adType === 'smallad') {
+                                $('#offer-info').addClass('hidden');
+                            }
+                            else if (adType === 'offer') {
+                                $('#offer-info').removeClass('hidden');
+                            }
+                        });
+
                     $('#post-ad-form').on('submit', function(e) {
                         e.preventDefault();
 
+                        adType = $('#select-ad-type').val();
                         const category = $('#select-ad-category').val();
                         const title = $('#ad-title').val();
                         const description = $('#ad-description').val();
+                        const price = $('#offer-price').val();
 
-                        let smallAd = new SmallAd(title, description);
-                        console.log('smallAd ', smallAd);
-                        let adWithCategory = {
-                            category: category,
-                            title: smallAd.title,
-                            description: smallAd.description
+                        let newAd,
+                            Ad;
+                        if (adType === 'smallad') {
+                            Ad = new SmallAd(title, description);
+                            console.log('Ad smallad', Ad);
+                            newAd = {
+                                category: category,
+                                title: Ad.title,
+                                description: Ad.description
+                            }
                         }
-                        console.log('adWithCategory ', adWithCategory);
+                        else if (adType === 'offer') {
+                            Ad = new Offer(title, description, price);
+                            console.log('Ad offer', Ad);
+                            newAd = {
+                                category: category,
+                                title: Ad.title,
+                                description: Ad.description,
+                                price: Ad.price
+                            }
+                        }
                         
-                        data.classifieds.post(adWithCategory)
+                        data.classifieds.post(newAd)
                         .then(
                             result => {
                                 toastr.success(`New ad was posted`);
@@ -186,26 +204,13 @@ let TemplateController = (function() {
                             }
                         );
                     });
-                    
                 })
                 .catch(console.log);
         }
 
         loadCategory(categoryName) {
             this.hideLoginRegister();
-
-            data.classifieds.getCount()
-            .then((data) => {
-                Promise.all([data, tl.loadTemplate('top')])
-                    .then(([data, template]) => $('#top-container').html(template(data)))
-                    .then(() => {
-                        $('#categories').change(function () {
-                            let category = $(this).val();
-                            let url = '/categories/' + category;
-                            window.location.hash = url;
-                        });
-                    });
-            });
+            this.loadTopContainer();
 
             data.categories.get(categoryName)
             .then((data) => {
@@ -216,27 +221,59 @@ let TemplateController = (function() {
         }
 
         loadAd(id) {
+            $("#add-new-comment-btn").off();
+            $("#submit-comment").off();
+            let self = this;
             this.hideLoginRegister();
-
-            data.classifieds.getCount()
-            .then((data) => {
-                Promise.all([data, tl.loadTemplate('top')])
-                    .then(([data, template]) => $('#top-container').html(template(data)))
-                    .then(() => {
-                        $('#categories').change(function () {
-                            let category = $(this).val();
-                            let url = '/categories/' + category;
-                            window.location.hash = url;
-                        });
-                    });
-            });
+            this.loadTopContainer();
 
             data.classifieds.get(id)
-            .then((data) => {
-                Promise.all([data, tl.loadTemplate('singlead')])
-                    .then(([data, template]) => $('#main').html(template(data)))
+            .then((result) => {
+                Promise.all([result, tl.loadTemplate('singlead')])
+                    .then(([result, template]) => $('#main').html(template(result)))
+                    .then(() => {
+                        $(function() {
+                            $("#comment-dialog").dialog({
+                                autoOpen: false,
+                                maxWidth:600,
+                                maxHeight: 400,
+                                width: 480,
+                                height: 330,
+                            });
+                            $("#add-new-comment-btn").on("click", function() { 
+                                $("#comment-dialog").dialog("open");
+                            });
+                        });
+                        
+                        $("#submit-comment").on('click', function(e) {
+                            e.preventDefault();
+                            $("#comment-dialog").dialog("close");
+
+                            let commentText = $("#comment-text").val();
+                            let newComment = new Comment(commentText);
+
+                            let adId = window.location.hash.replace(/\D/g, '');
+
+                            let newCommentWithId = {
+                                id: adId,
+                                text: newComment.text
+                            };
+
+                            data.classifieds.postComment(newCommentWithId)
+                            .then(
+                                result => {
+                                    toastr.success(`Your comment was posted successfully`);
+                                    self.loadAd(id);
+                                },
+                                errorMsg => {
+                                    toastr.error(errorMsg.responseJSON);
+                                }
+                            );
+                        });
+                    })
                     .catch(console.log);
             })
+            
         }
     }
 
